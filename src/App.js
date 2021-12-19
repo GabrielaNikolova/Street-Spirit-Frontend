973a2517ddb0821c94923dca155e8992c9e4824e
GabrielaNikolova <gnikolova@ymail.com>
2021-12-20 01:11:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div id="container">
+
+            <main id="site-content">
+
+            </main>
+        </div>
+    );
+}
+
+export default App;
